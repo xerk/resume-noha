50b69fe184ea773644f4b4a2ab1ce5f3bd6604bf
Ahmed Mamdouh <gm.xerk@gmail.com>
2024-12-26 00:40:28 +0200

    feat: Update metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,15 +15,37 @@ const fontSans = FontSans({
 export const metadata: Metadata = {
   metadataBase: new URL(DATA.url),
   title: {
-    default: DATA.name,
+    default: `${DATA.name} | ${DATA.role}`,
     template: `%s | ${DATA.name}`,
   },
   description: DATA.description,
+  keywords: [
+    "Ahmed Mamdouh",
+    "Software Engineer",
+    "Full Stack Developer",
+    "Laravel",
+    "Next.js",
+    "TypeScript",
+    "Node.js",
+    "Vue.js",
+    "React",
+    ...DATA.skills
+  ],
+  authors: [{ name: DATA.name, url: DATA.url }],
+  creator: DATA.name,
   openGraph: {
-    title: `${DATA.name}`,
+    title: `${DATA.name} | ${DATA.role}`,
     description: DATA.description,
     url: DATA.url,
-    siteName: `${DATA.name}`,
+    siteName: DATA.name,
+    images: [
+      {
+        url: `${DATA.url}/og-image.png`,
+        width: 1200,
+        height: 630,
+        alt: DATA.name,
+      }
+    ],
     locale: "en_US",
     type: "website",
   },
@@ -39,12 +61,17 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: `${DATA.name}`,
     card: "summary_large_image",
+    title: `${DATA.name} | ${DATA.role}`,
+    description: DATA.description,
+    creator: "@xerk",
+    images: [`${DATA.url}/og-image.png`],
   },
   verification: {
-    google: "",
-    yandex: "",
+    google: "RB2dzZGLiNJe7uPzE0s-vpARRoG0ZGv_6mOFsIlFAf4",
+  },
+  alternates: {
+    canonical: DATA.url,
   },
 };
 
